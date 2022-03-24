93605e5b0ce840a8af99f4ebec44039c991f388c
hamelsmu <hamel.husain@gmail.com>
2022-03-24 15:11:39 -0700

    update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -71,7 +71,7 @@ const config = {
             items: [
               {
                 label: 'Tutorial',
-                to: '/docs/Docusaurus/intro',
+                to: '/docs/nb',
               },
             ],
           },
