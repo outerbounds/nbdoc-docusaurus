4a8c490b29cf18bf1c648dd7534957531cf02ed3
hamelsmu <hamel.husain@gmail.com>
2022-03-24 22:27:35 -0700

    improve landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -34,7 +34,45 @@ export default function Home() {
       <HomepageHeader />
       <main>
       <center>
+      <h1>Create Technical Documentation That...</h1>
+      <p><font size="+1"><strong>Is fully testable</strong> with unit tests</font></p>
+      <p><font size="+1"><strong>Doesn't require copy & pasting code</strong> into markdown files.</font></p>
+      <p><font size="+1"><strong>Can automatically update</strong> by programatically running the code in your docs.</font></p>
+      <p><font size="+1">Allows you to author docs <strong>using a WSIWYG style editor</strong>.</font></p>
+      <p><font size="+1">Leverages <a href="https://docusaurus.io/">Docusaurus</a>, a popular static site generator built specifically for documentation.</font></p>
+      <br></br>
+      <br></br>
       <img width="949" alt="nbdoc4" src="https://user-images.githubusercontent.com/1483922/159776255-3eb05cd5-4d48-48c4-80e7-7825839da0ef.png"></img>
+      <h1>A Whirlwind Tour</h1>
+      <h2>Hello World</h2>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/zclB89-exZ4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <br></br>
+      <br></br>
+      <h2>Write Python Scripts and Shell Commands</h2>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/LP8w-JRi84A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <br></br>
+      <br></br>
+      <h2>Toggle Visibility of Cell Inputs & Outputs</h2>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/I2AA2N-kVl0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <br></br>
+      <br></br>
+      <h2>Create Interactive Plots</h2>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/ODPXd8xghzk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <br></br>
+      <br></br>
+      <h2>Test Documentation</h2>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/vhA5WQiZ3wY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <br></br>
+      <br></br>
+      <h1>Ready To Get Started?  Take The Tutorial!</h1>
+      <div className={styles.buttons}>
+        <font size="+5">👉&nbsp;</font>
+          <Link
+            className="button button--secondary button--lg"
+            to="/docs/nb">
+            nbdoc Tutorial - 15min ⏱️
+          </Link>
+      </div>
       </center>
       </main>
     </Layout>
